3f2e1697b0d2f5d53b8f9510c52cb3b1d8c4a763
Marcin Brach <marcin.brc@gmail.com>
2018-01-26 15:46:01 +0100

    faux dom implementation

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { select } from "d3-selection";
 import { geoPath, geoMercator } from "d3-geo";
-
+import { withFauxDOM } from "react-faux-dom";
 
 class Map extends Component {
     constructor(props) {
@@ -11,6 +12,12 @@ class Map extends Component {
 
         this.path = geoPath()
             .projection(proj);
+
+        this.faux = this.props.connectFauxDOM("g", "chart");
+    }
+
+    componentDidMount() {
+        this.updateMap();
     }
 
     componentWillUpdate(props) {
@@ -22,21 +29,42 @@ class Map extends Component {
             .projection(proj);
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.scale !== prevProps.scale) {
+            this.updateMap()
+        }
+    }
+
+    updateMap() {
+        const { scale, data } = this.props;
+        const proj = geoMercator()
+            .scale(scale);
+
+        this.path = geoPath()
+            .projection(proj);
+
+        select(this.faux)
+            .selectAll(".country").remove();
+
+        select(this.faux)
+            .selectAll(".country")
+            .data(data.features)
+            .enter()
+            .append("path")
+            .attr("d", this.path)
+            .attr("class", (d) => `country ${d.id}`)
+            .attr("fill", "blue");
+
+        this.props.animateFauxDOM(800);
+    }
+
     render() {
-        const { features } = this.props.data;
-        return (
-            <g>
-                {features.map(d => (
-                    <path
-                        key={d.id}
-                        className={`country ${d.id}`}
-                        d={this.path(d)}
-                        fill="blue"
-                    />
-                ))}
-            </g>
-        );
+        return this.props.chart;
     }
 }
 
-export default Map;
+Map.defaultProps = {
+    chart: "loading"
+};
+
+export default withFauxDOM(Map);
